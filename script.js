1acdf0e5bc29c751d17903404de55529e32fd9d4
laita <laita.kamila32@smk.belajar.id>
2026-01-29 09:20:48 +0700

    music

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function buka() {
     window.location.href = "halaman2.html";
+   
 }
+
 function playmusic() {
-    const audio = document.getElementById("music");
-    audio.play();
+    document.getElementById("music").play();
 }
